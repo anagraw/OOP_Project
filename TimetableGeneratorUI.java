0f1847314d0381f78a3a71eac87d22dc29dd616c
Surya743 <gsuryashashank@gmail.com>
2025-04-19 14:08:49 +0000

    add calendar

--- TimetableGeneratorUI.java
+++ TimetableGeneratorUI.java
@@ -1,6 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import com.davidmoodie.SwingCalendar.Calendar;
+import com.davidmoodie.SwingCalendar.CalendarEvent;
+import com.davidmoodie.SwingCalendar.WeekCalendar;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 
 public class TimetableGeneratorUI {
     public static void main(String[] args) {
@@ -49,27 +56,109 @@ public class TimetableGeneratorUI {
             card.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
             card.setBackground(Color.WHITE);
             card.setPreferredSize(new Dimension(150, 150));
-            
+
             JLabel label = new JLabel(timetable, SwingConstants.CENTER);
             label.setFont(new Font("Arial", Font.BOLD, 14));
-            
+
             JButton viewEditButton = new JButton("View and Edit");
             viewEditButton.setBackground(Color.BLACK);
             viewEditButton.setForeground(Color.WHITE);
             viewEditButton.setFocusPainted(false);
             viewEditButton.setBorderPainted(false);
-            
-            viewEditButton.addActionListener(e -> {
-                JFrame newFrame = new JFrame(timetable);
-                newFrame.setSize(800, 500);
-                newFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                newFrame.add(new JLabel(timetable, SwingConstants.CENTER));
-                newFrame.setVisible(true);
+
+            viewEditButton.addActionListener(event -> {
+                JFrame frm = new JFrame();
+
+                ArrayList<CalendarEvent> events = new ArrayList<>();
+                // Week of 14 Apr 2025
+                events.add(new CalendarEvent(LocalDate.of(2025, 4, 14), LocalTime.of(8, 0), LocalTime.of(8, 50),
+                        "ECE F343 - T1 Tutorial\nI BLOCK I112"));
+                events.add(new CalendarEvent(LocalDate.of(2025, 4, 14), LocalTime.of(9, 0), LocalTime.of(9, 50),
+                        "CS F213 - L1 Lecture\nF BLOCK F106"));
+                events.add(new CalendarEvent(LocalDate.of(2025, 4, 14), LocalTime.of(10, 0), LocalTime.of(10, 50),
+                        "CS F372 - L1 Lecture\nF BLOCK F104"));
+                events.add(new CalendarEvent(LocalDate.of(2025, 4, 14), LocalTime.of(11, 0), LocalTime.of(11, 50),
+                        "ECE F341 - L2 Lecture\nF BLOCK F104"));
+                events.add(new CalendarEvent(LocalDate.of(2025, 4, 14), LocalTime.of(12, 0), LocalTime.of(12, 50),
+                        "ECE F343 - L1 Lecture\nF BLOCK F104"));
+                events.add(new CalendarEvent(LocalDate.of(2025, 4, 14), LocalTime.of(14, 0), LocalTime.of(14, 50),
+                        "ECE F344 - L1 Lecture\nF BLOCK F106"));
+
+                events.add(new CalendarEvent(LocalDate.of(2025, 4, 15), LocalTime.of(9, 0), LocalTime.of(9, 50),
+                        "ECE F344 - L1 Lecture\nF BLOCK F106"));
+
+                events.add(new CalendarEvent(LocalDate.of(2025, 4, 16), LocalTime.of(8, 0), LocalTime.of(8, 50),
+                        "ECE F344 - T2 Tutorial\nI BLOCK I113"));
+                events.add(new CalendarEvent(LocalDate.of(2025, 4, 16), LocalTime.of(9, 0), LocalTime.of(9, 50),
+                        "CS F213 - L1 Lecture\nF BLOCK F106"));
+                events.add(new CalendarEvent(LocalDate.of(2025, 4, 16), LocalTime.of(10, 0), LocalTime.of(10, 50),
+                        "ECON F315 - L1 Lecture\nJ BLOCK J115"));
+                events.add(new CalendarEvent(LocalDate.of(2025, 4, 16), LocalTime.of(11, 0), LocalTime.of(12, 50),
+                        "ECE F341 - P4 Laboratory\nJ BLOCK J106"));
+                events.add(new CalendarEvent(LocalDate.of(2025, 4, 16), LocalTime.of(12, 0), LocalTime.of(12, 50),
+                        "ECE F343 - L1 Lecture\nF BLOCK F104"));
+
+                events.add(new CalendarEvent(LocalDate.of(2025, 4, 17), LocalTime.of(9, 0), LocalTime.of(9, 50),
+                        "ECE F344 - L1 Lecture\nF BLOCK F106"));
+                events.add(new CalendarEvent(LocalDate.of(2025, 4, 17), LocalTime.of(10, 0), LocalTime.of(10, 50),
+                        "ECON F315 - L1 Lecture\nJ BLOCK J115"));
+                events.add(new CalendarEvent(LocalDate.of(2025, 4, 17), LocalTime.of(11, 0), LocalTime.of(12, 50),
+                        "CS F213 - P2 Laboratory\nD BLOCK D311"));
+                events.add(new CalendarEvent(LocalDate.of(2025, 4, 17), LocalTime.of(14, 0), LocalTime.of(14, 50),
+                        "ECE F341 - T2 Tutorial\nI BLOCK I122"));
+
+                events.add(new CalendarEvent(LocalDate.of(2025, 4, 18), LocalTime.of(9, 0), LocalTime.of(9, 50),
+                        "CS F213 - L1 Lecture\nF BLOCK F106"));
+                events.add(new CalendarEvent(LocalDate.of(2025, 4, 18), LocalTime.of(10, 0), LocalTime.of(10, 50),
+                        "CS F372 - L1 Lecture\nF BLOCK F104"));
+                events.add(new CalendarEvent(LocalDate.of(2025, 4, 18), LocalTime.of(11, 0), LocalTime.of(11, 50),
+                        "ECE F341 - L2 Lecture\nF BLOCK F104"));
+                events.add(new CalendarEvent(LocalDate.of(2025, 4, 18), LocalTime.of(12, 0), LocalTime.of(12, 50),
+                        "ECE F343 - L1 Lecture\nF BLOCK F104"));
+
+                events.add(new CalendarEvent(LocalDate.of(2025, 4, 19), LocalTime.of(17, 0), LocalTime.of(17, 50),
+                        "ECON F315 - L1 Lecture\nJ BLOCK J115"));
+
+                WeekCalendar cal = new WeekCalendar(events);
+
+                cal.addCalendarEventClickListener(e -> System.out.println(e.getCalendarEvent()));
+                cal.addCalendarEmptyClickListener(e -> {
+                    System.out.println(e.getDateTime());
+                    System.out.println(Calendar.roundTime(e.getDateTime().toLocalTime(), 30));
+                });
+
+                JButton goToTodayBtn = new JButton("Today");
+                goToTodayBtn.addActionListener(e -> cal.goToToday());
+
+                JButton nextWeekBtn = new JButton(">");
+                nextWeekBtn.addActionListener(e -> cal.nextWeek());
+
+                JButton prevWeekBtn = new JButton("<");
+                prevWeekBtn.addActionListener(e -> cal.prevWeek());
+
+                JButton nextMonthBtn = new JButton(">>");
+                nextMonthBtn.addActionListener(e -> cal.nextMonth());
+
+                JButton prevMonthBtn = new JButton("<<");
+                prevMonthBtn.addActionListener(e -> cal.prevMonth());
+
+                JPanel weekControls = new JPanel();
+                weekControls.add(prevMonthBtn);
+                weekControls.add(prevWeekBtn);
+                weekControls.add(goToTodayBtn);
+                weekControls.add(nextWeekBtn);
+                weekControls.add(nextMonthBtn);
+
+                frm.add(weekControls, BorderLayout.NORTH);
+
+                frm.add(cal, BorderLayout.CENTER);
+                frm.setSize(1000, 900);
+                frm.setVisible(true);
             });
-            
+
             card.add(label, BorderLayout.CENTER);
             card.add(viewEditButton, BorderLayout.SOUTH);
-            
+
             contentPanel.add(card);
         }
 
